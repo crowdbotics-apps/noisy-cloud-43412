10ccdfa302a65cf610bf0f2373cf5ba3e1501f70
Crowdbotics <team@crowdbotics.com>
2023-09-16 04:35:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-cloud-43412",
+  url: "https://noisy-cloud-43412.botics.co",
+  api: "https://noisy-cloud-43412.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
